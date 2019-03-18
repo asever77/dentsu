e1f94f85a00789226056c6eec470d924e8daf508
SMILEGATE\es-mizushiro <es-mizushiro@smilegate.com>
2019-03-18 17:37:27 +0900

    수정 요청

--- resources/js/ui.common.js
+++ resources/js/ui.common.js
@@ -91,6 +91,17 @@
         }
     };
 
+    // fadeinup
+    $(window).scroll( function(){
+        $('.fadeinup').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).addClass('active');
+            }
+        });
+    });
+
     //modal
     
 
